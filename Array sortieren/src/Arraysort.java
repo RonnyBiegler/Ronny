3c58ec0ce177f718583f0d1e5a30cb5737716512
ronnybiegler <ronny.biegler@gmail.com>
2014-12-11 11:40:17 +0100

    sortieren

--- Array sortieren/src/Arraysort.java
+++ Array sortieren/src/Arraysort.java
@@ -2,32 +2,55 @@
 
 public class Arraysort {
 	public static void main(String[] args) {
-		
+	
+	Arraysort as = new Arraysort();
+	int[] array = {5,2,3,4,7,6,72,9,10};
+	
+	
+	as.sortiere(array);
+	
+	
+	
+	
+	
+	for (int i=0; i<array.length;i++) {
+		System.out.println(array[i]);	
+	}
 	
 	
 	}	
 	
-	int[] array = {5,2,3,4,7,6,72,9,10};
-	public void sortiere (int[] array) {
+	
+	public void sortiere (int[] array) {				// Zahlen sortieren
 		boolean tausch = true;
-		while (tausch) {								// Anlegen der Überprüfung ob Tausch stattgefunden hat 
+		for (int j = 1; tausch;j++) {					// Anlegen der Überprüfung ob Tausch stattgefunden hat 
 		tausch = false;
-			for( int i =0; i<array.length-1;i++) {		// Hilfsvariable zum Taschen, -1 da nicht übers gesamte Array gesucht werden darf, es müssen immer 2 Zahlen miteinander geprüft werden
+			for( int i =0; i<array.length-j;i++) {		// Hilfsvariable zum Taschen, -1 da nicht übers gesamte Array gesucht werden darf, es müssen immer 2 Zahlen miteinander geprüft werden
 			if (array[i] > array[i+1]) {				// über generellen Tausch
 				int help = array[i];					//1
 				array[i] = array[i+1];					//2
 				array[i+1] = help;						//3
 				tausch = true;
-				
-				System.out.println(" "+ array[i]);
-				
-					
+													
 				} 
 			} 
 			
 		} 
 		
 	}
+	
+	public boolean lexikografisch (String t1, String t2) {				// lexikografisches Sortieren
+		String s1 = t1.toUpperCase();
+		String s2 = t2.toUpperCase();
+		for (int i = 0; i<s1.length() && i<s2.length();i++) 
+			if (s1.charAt(i) > s2.charAt(i))	
+					return true;
+			else if (s1.charAt(i) < s2.charAt(i))
+					return false;
+		return s1.length() >s2.length();
+		
+	}
+	
 	
 	
 
--- Vergleichen/src/Testmethoden.java
+++ Vergleichen/src/Testmethoden.java
@@ -3,9 +3,10 @@ public class Testmethoden {
 
 	public static void main(String[] args) {
 		Hilfmethoden hm = new Hilfmethoden();
-			int [] array = {1,2,3,4,5,6,7,8,9,10};
+			int [] array = {1,2,3,4,5,6};
 			System.out.println(hm.vergleichen("Hallo","Hallo") + "\n");
-			System.out.println(hm.summe(array)); 
+			System.out.println(hm.summe(array) + "\n"); 
+			System.out.println(hm.schnitt(array));
 	}
 
 }
